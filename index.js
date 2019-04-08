c9a99b53de42b7fa8010e65ba5108927fd44caf5
walksa <miwanishioka701@gmail.com>
2019-04-08 20:44:54 +0800

    [added] new files

---
+++ index.js
@@ -0,0 +1,43 @@
+function copyArea(textArea){
+  const dt = new Date()
+  const dtStr = dt.toLocaleString()
+	const txtStr = document.getElementById(textArea).value
+  str = `[${dtStr}] : ${txtStr}`
+  copyText(str)
+}
+
+function clearArea(textArea){
+	document.getElementById(textArea).value = ""
+}
+
+function copyText(str){
+	if(execCopy(str)){
+		alert('コピーできました\n'+"-----------------------\n"+str)
+	}else {
+		alert('このブラウザでは対応していません')
+	}
+}
+
+function execCopy(string){
+  const temp = document.createElement('div')
+
+  temp.appendChild(document.createElement('pre')).textContent = string
+
+  let s = temp.style
+  s.position = 'fixed'
+  s.left = '-100%'
+
+  document.body.appendChild(temp)
+  document.getSelection().selectAllChildren(temp)
+
+  const result = document.execCommand('copy')
+
+  document.body.removeChild(temp)
+  return result
+}
+
+function clearAll(){
+	document.getElementById("textarea1").value = ""
+	document.getElementById("textarea2").value = ""
+	document.getElementById("textarea3").value = ""
+}
